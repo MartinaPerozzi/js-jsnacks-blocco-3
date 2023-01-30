c790b6ae94655f642b822668c50c037bd1f18bdf
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-30 14:26:32 +0100

    Snack1 - html + css + js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Preparare gli elementi necessari //
+const submitButton = document.querySelector(".submit-button");
+
+/* Chiedere all'utente un numero massimo e un numero minimo*/
+submitButton.addEventListener(
+    "click",
+    function () {
+        const minValue = document.querySelector(".min-number").value;
+        console.log("min" + minValue)
+        const maxValue = document.querySelector(".max-number").value;
+        console.log("max" + maxValue)
+
+        if ((maxValue <= minValue) && (maxValue <= 0 || minValue <= 0)) {
+            document.querySelector(".mess").innerHTML = "Inserisci i dati correttamente."
+
+        } if (minValue < maxValue) {
+
+            // Problema con la generazione del numero randomico
+
+            const betweenNumber = Math.floor(Math.random() * (maxValue - minValue) + minValue);
+
+            document.querySelector(".mess").innerHTML = `Numero generato tra ${minValue}  e  ${maxValue} = ${betweenNumber}`;
+            console.log(betweenNumber)
+
+        }
+    }
+
+)
