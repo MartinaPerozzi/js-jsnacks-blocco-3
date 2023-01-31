110807609d2d4ae4b9416d82d6d694e601473a46
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-31 13:32:20 +0100

    Snack3- refactoring + Snack4 (after lesson)

---
+++ Snack4 Bonus/js/script.js
@@ -0,0 +1,21 @@
+
+let numberValue_1 = parseInt(prompt("Inserisci un numero"));
+
+let numberValue_2 = parseInt(prompt("Inserisci un altro numero"));
+
+// Finchè N sarà minore o uguale nel valore inserito dall'utente
+while (numberValue_2 > numberValue_1) {
+
+    numberValue_1 = numberValue_2;
+
+    numberValue_2 = parseInt(prompt("Inserisci un altro numero"));
+
+
+    console.log(numberValue)
+
+
+}
+
+
+
+
